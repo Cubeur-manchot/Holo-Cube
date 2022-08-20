46f663c65f08c62ae2630ba63e14e2070236ab5d
Cubeur-manchot <matthieu.aubert17@gmail.com>
2022-08-20 22:09:31 +0200

    Script update for prefill

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const inputIsKo = (textareaTag, exception) => {
 
 const prefillWithExample = () => {
 	let textareaTag = document.querySelector("textarea#jsonInput");
-	textareaTag.innerHTML = '{\n    "puzzle": {\n        "fullName": "cube4x4x4"\n    },\n    "moveSequence": "Fw"\n,    "verbosity": 1\n}';
+	textareaTag.innerHTML = '{\n    "puzzle": {\n        "fullName": "cube4x4x4"\n    },\n    "moveSequence": "Rw U R\' U\' 2R\' U R U\' R\'",\n    "verbosity": 1\n}';
 	inputIsOk(textareaTag);
 };
 
